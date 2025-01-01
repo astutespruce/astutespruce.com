6df1154df98f2e84a9de1a5682786f3f1e76ecec
Brendan C. Ward <bcward@astutespruce.com>
2025-01-01 09:02:19 -0800

    Update error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,8 +1,7 @@
 <script>
-    import TriangeAlert from 'lucide-svelte/icons/triangle-alert'
     import { page } from '$app/state'
     import { ImageCredit } from '$lib/components/image'
-    import NotFoundImage from '$lib/assets/images/deanna-ritchie-227649-unsplash.jpg'
+    import NotFoundImage from '$lib/assets/images/deanna-ritchie-227649-unsplash.jpg?enhanced'
 
     console.error(page.status)
     console.error(page.error)
@@ -13,28 +12,30 @@
 </svelte:head>
 
 {#if page.status === 404}
-    <div class="h-full w-full bg-cover py-12" style={`background-image: url(${NotFoundImage});`}>
-        <div
-            class="container max-w-[700px] border-2 border-foreground bg-muted p-12"
-            style="box-shadow: 1px 1px 0.5rem #000;"
-        >
-            <h1 class="text-6xl font-bold">PAGE NOT FOUND</h1>
-            <div class="mt-10 text-4xl">You appear to be lost...</div>
-        </div>
-    </div>
-    <ImageCredit author="Deanna Ritchie" url="https://unsplash.com/photos/wORTURlz7jg" />
-{:else}
-    <div class="w-full py-12">
-        <div class="mx-auto mt-10 max-w-[540px] border-4 border-accent-foreground/50 bg-muted p-12">
-            <h1 class="flex items-center gap-8 text-7xl font-bold">
-                <TriangeAlert size="4rem" class="text-muted-foreground" />
-                Uh oh!
-            </h1>
-            <div class="mt-4 text-3xl">There was an unexpected error</div>
-            <div class="mt-4 text-xl text-muted-foreground">
-                Please try again in a few minutes. If that still doesn't work, please
-                <a href="mailto:bcward@astutespruce.com"> let us know</a>!
+    <div class="hidden sm:block">
+        <div class="relative z-0 h-max w-full overflow-hidden">
+            <enhanced:img src={NotFoundImage} alt="signpost" class="z-1 relative" />
+            <div class="z-2 absolute bottom-0 left-0 right-0 top-10">
+                <div class=" container border-foreground p-12 sm:border-2 sm:bg-muted md:max-w-[700px]">
+                    <h1>PAGE NOT FOUND</h1>
+                    <div class="mt-4 text-2xl md:mt-10 md:text-4xl">You appear to be lost...</div>
+                </div>
             </div>
+        </div>
+        <ImageCredit author="Deanna Ritchie" url="https://unsplash.com/photos/wORTURlz7jg" />
+    </div>
+
+    <div class="container mt-10 sm:hidden">
+        <h1>PAGE NOT FOUND</h1>
+        <div class="mt-4 text-2xl md:mt-10 md:text-4xl">You appear to be lost...</div>
+    </div>
+{:else}
+    <div class="container my-20">
+        <h1>Uh oh!</h1>
+        <h2>There was an unexpected error</h2>
+        <div class="mt-4 text-xl text-muted-foreground">
+            Please try again in a few minutes. If that still doesn't work, please
+            <a href="mailto:bcward@astutespruce.com"> let us know</a>!
         </div>
     </div>
 {/if}
